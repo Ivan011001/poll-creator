da147f7c33b09e1ca7cf871fe95f469c8c8483fa
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-26 18:07:45 +0200

    fix: app init

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,23 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const logger = new Logger('Main (main.ts)');
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'http://localhost:8080',
-        /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):8080$/,
-      ],
-    },
-  });
-
-  await app.init();
+  const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
+  const clientPort = configService.get('CLIENT_PORT');
   const port = parseInt(configService.get('PORT'));
+
+  app.enableCors({
+    origin: [
+      `http://localhost:${clientPort}`,
+      new RegExp(`/^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$/`),
+    ],
+  });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
 
   logger.log(`Server running on port ${port}`);
