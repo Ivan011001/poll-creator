19cd4b302cf9690310d1a0481d76bd850b02321b
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-27 20:33:09 +0200

    feat: shared types

---
+++ server/src/polls/types.ts
@@ -0,0 +1,32 @@
+//service interfaces
+export interface CreatePollFields {
+  topic: string;
+  votesPerVoter: number;
+  name: string;
+}
+
+export interface JoinPollFields {
+  pollID: string;
+  name: string;
+}
+
+export interface RejoinPollField {
+  pollID: string;
+  userID: string;
+  name: string;
+}
+
+//repository interfaces
+
+export interface CreatePollData {
+  pollID: string;
+  topic: string;
+  votesPerVoter: number;
+  userID: string;
+}
+
+export interface AddParticipantData {
+  pollID: string;
+  userID: string;
+  name: string;
+}
