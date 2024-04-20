bc00eea865b0cdd3255192f09f582c8705cbb855
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-04-20 16:40:06 +0300

    feat: redux persist

---
+++ client/src/components/ui/loader.tsx
@@ -0,0 +1,13 @@
+import { Oval } from "react-loader-spinner";
+
+const Loader = () => {
+  return (
+    <Oval
+      color="#DD6B20"
+      secondaryColor="#FFA94D"
+      wrapperClass="h-full flex justify-center items-center"
+    />
+  );
+};
+
+export default Loader;
--- client/src/providers/store-provider.tsx
+++ client/src/providers/store-provider.tsx
@@ -3,8 +3,12 @@
 import { useRef } from "react";
 
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 import { makeStore, AppStore } from "../redux/store";
+
+import Loader from "@/components/ui/loader";
 
 export default function StoreProvider({
   children,
@@ -16,5 +20,14 @@ export default function StoreProvider({
     storeRef.current = makeStore();
   }
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  return (
+    <Provider store={storeRef.current}>
+      <PersistGate
+        persistor={persistStore(storeRef.current)}
+        loading={<Loader />}
+      >
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
